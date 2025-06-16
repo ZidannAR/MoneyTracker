7dc2c88d305c619fdf94376a9f947e9b49d3d467
ZidannAR <zidanalrajib@gmail.com>
2025-06-16 10:40:51 +0700

    add page category

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,5 +1,6 @@
 import 'package:calendar_appbar/calendar_appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:uangko/pages/category_page.dart';
 import 'package:uangko/pages/home_page.dart';
 
@@ -11,9 +12,7 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  @override
-  Widget build(BuildContext context) {
-    final List<Widget> children = [HomePage(), CategoryPage()];
+    final List<Widget> _children = [HomePage(), CategoryPage()];
     int currentIndex = 0;
 
     void onTapTapped(int index) {
@@ -21,22 +20,30 @@ class _MainPageState extends State<MainPage> {
         currentIndex = index;
       });
     }
+  Widget build(BuildContext context) {
 
     return Scaffold(
-      appBar: CalendarAppBar(
+      appBar: (currentIndex == 0) ? CalendarAppBar(
         accent: Colors.green,
         backButton: false,
         locale: 'id',
         onDateChanged: (value) => print(value),
         firstDate: DateTime.now().subtract(Duration(days: 140)),
         lastDate: DateTime.now(),
+      ) : PreferredSize(preferredSize: Size.fromHeight(100), 
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 36, horizontal: 16),
+        child: Container(child: Text('categories',style: GoogleFonts.montserrat(fontSize: 20),),),
+      )),
+      floatingActionButton: Visibility(
+        visible: (currentIndex == 0)? true:false,
+        child: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.green,
+          child: Icon(Icons.add),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.green,
-        child: Icon(Icons.add),
-      ),
-      body: HomePage(),
+      body: _children[currentIndex],
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomAppBar(
         child: Row(
