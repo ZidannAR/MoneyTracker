9f5f52c35f24f847e5213277173bee88018e5d8f
ZidannAR <zidanalrajib@gmail.com>
2025-06-18 09:14:15 +0700

    melanjutkan ui category page

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -10,6 +10,40 @@ class CategoryPage extends StatefulWidget {
 class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Center());
+    return SafeArea(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Switch(
+                  value: true,
+                  onChanged: (bool value) {},
+                  activeColor: Colors.red, // thumb
+                  activeTrackColor: Colors.red[200], // track
+                  // warna saat OFF
+                  inactiveThumbColor: Colors.green, // thumb
+                  inactiveTrackColor: Colors.green[200],
+                ),
+                IconButton(onPressed: () {}, icon: Icon(Icons.add))
+              ],
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.upload, color: Colors.red,),
+            title: Text('sedekah'),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(onPressed: (){}, icon: Icon(Icons.delete)),
+                IconButton(onPressed: (){}, icon: Icon(Icons.edit))
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
